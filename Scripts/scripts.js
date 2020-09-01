12e7fb23d00518d626b731f887dd3a22bbca199e
Patrick W <pat.realrealestatellc@gmail.com>
2020-09-01 17:52:19 -0400

    Prompt that asks character length is set up

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,51 +1,34 @@
 /* Defining charset variables */
-var letters = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]; 
+var lowerCase = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]; 
+var upperCase = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]; 
 var numbers = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 0];
 var special = ["!" , "@" , "#" , "$" , "%" , "^" , "&" , "*" , "(" , ")" , "{" ,  "}" , "[" , "]" , "<" , ">" , "/" , "?"];
 
 /* Creating var to hold the value of the password length  */
 var pwLength = 0;
 
-/* Creating array to hold charater type values */
-
+/* Creating a var to hold userChoice values */
 var userChoice = 0;
 
+
 /* Creating a function to activate generator when button is clicked */
-function generateBtn() {
-    pwLength = prompt("Choose password length between 8 and 128 character");
+function generatorBtn() {
 
-/* Conditional setting alert if user chooses pwLength less than 8 characters */
-    if (pwLength < 8) {
-        alert("Password must be at least 8 characters long");
-        pwLength = prompt("Choose password length between 8 and 128 character");
-    } else {
-        pwLength = pwLength;
+/* Looping conditional to alert user anytime charset is less than 8 and more than 128 */
+
+    for (var i = 0; i < 8; i++) {
+
+        pwLength = prompt("Choose password length between 8 and 128 characters");
+
+        if (pwLength < 8) {
+            alert("Password must be at least 8 characters long");
+            i = 0;
+        } else if (pwLength > 128) {
+                alert("Password cannot exceed 128 characters");
+                i = 0;                
+        } else {
+            pwLength = pwLength;
+            i = 8;
+        }
     }
-    
-/* Conditional setting alert if user choose pwLength more than 128 characters */
-    if (pwLength > 128) {
-        alert("Password cannot exceed 128 characters");
-        pwLength = prompt("Choose password length between 8 and 128 character");
-    } else {
-        pwLength = pwLength;
-    }
-
-/* Nothing below this line is working. Figure the logic out tomorrow. Figure out how to get the userChoice prompt to work. */
-
-
-
-
-/* Prompt for user to choose character types used in random password */
-userChoice = prompt("Choose what kind of characters to include in your password: numbers, special, lowercase, uppercase");
-
-    /* Conditional to define userCharChoice */
-    if (userChoice !== "numbers" || "special" || "lowercase" || "uppercase" || "all") {
-        alert("You must choose one of the listed");
-        userChoice = prompt("Choose what kind of characters to include in your password: numbers, special, lowercase, uppercase");
-    } else {
-        userChoice = userChoice;
-    }
-
-        console.log(userChoice);
 }
-
