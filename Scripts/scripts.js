9d6c2fea22b4bc63cf62ab7d9d721e55d5057546
Patrick W <pat.realrealestatellc@gmail.com>
2020-09-01 23:48:02 -0400

    Still adding to the character selection prompt

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -2,13 +2,14 @@
 var lowerCase = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]; 
 var upperCase = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]; 
 var numbers = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 0];
-var special = ["!" , "@" , "#" , "$" , "%" , "^" , "&" , "*" , "(" , ")" , "{" ,  "}" , "[" , "]" , "<" , ">" , "/" , "?"];
+var symbols = ["!" , "@" , "#" , "$" , "%" , "^" , "&" , "*" , "(" , ")" , "{" ,  "}" , "[" , "]" , "<" , ">" , "/" , "?"];
 
 /* Creating var to hold the value of the password length  */
 var pwLength = 0;
 
 /* Creating a var to hold userChoice values */
-var userChoice = 0;
+// var choices = ["lowercase" , "uppercase" , "numbers" , "symbols"];
+var userCharSet = 0;
 
 
 /* Creating a function to activate generator when button is clicked */
@@ -31,4 +32,21 @@ function generatorBtn() {
             i = 8;
         }
     }
+
+/* Looping the conditional used for the character set alert */
+    for (var j = 0; j < 8; j++) {
+
+        userCharSet = prompt("Choose one or more of the following characters to be included in your password: lowercase, uppercase, numbers, symbols");
+
+        if (userCharSet !== "lowercase" || userCharSet !== "uppercase"  || userCharSet !== "numbers" || userCharSet !== "symbols") {
+            alert("Please choose one of the listed options. Press \"OK\" to continue.");
+            j = 0;
+        } else { 
+            userCharSet = "lowercase" || userCharSet == "uppercase"  || userCharSet == "numbers" || userCharSet == "symbols";
+            userCharSet = userCharSet;
+            j = 8;
+        }
+    }
 }
+
+/* Refer back to JavaScript folder for help tomorrow! */
