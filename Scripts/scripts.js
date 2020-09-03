2c688a63643ab57b47f17a1c7e6d7d635618b3cf
Patrick W <pat.realrealestatellc@gmail.com>
2020-09-02 22:20:50 -0400

    adding more files

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -23,8 +23,8 @@ function generatorBtn() {
             alert("Password must be at least 8 characters long");
             i = 0;
         } else if (pwLength > 128) {
-                alert("Password cannot exceed 128 characters");
-                i = 0;                
+            alert("Password cannot exceed 128 characters");
+            i = 0;
         } else {
             pwLength = pwLength;
             i = 8;
@@ -33,18 +33,24 @@ function generatorBtn() {
 
 /* Looping the conditional used for the character set alert */
     for (var j = 0; j < 8; j++) {
-        
-        userChoice = prompt("Choose one or more of the following for your password character set: lowercase, uppercase, numbers, symbols.");        
+           
+        userChoice = prompt("Choose one or more of the following choices for you password: lowercase, uppercase, numbers, symbols.");
         var userChoiceLowerCase = userChoice.toLowerCase();
 
-        if (userChoiceLowerCase.charAt(0) == "l" , "u" , "n" , "s"); {
+        if (userChoice = 0) {
+            j = 8;
+        } if (userChoiceLowerCase !== "lowercase" , "uppercase" , "numbers" , "symbols") {
+            alert("Please choose one of the listed.");
+            j = 0;
+        } else {
             userChoiceLowerCase = userChoiceLowerCase;
             j = 8;
-        } else { 
-            alert("Please choose one of the listed. Press \*OK\* to continue.");
-            j = 0;
-        }        
+        }
     }
+
+
+    // console.log(charAt(0));
+
 }
 
 /* Refer back to JavaScript folder for help tomorrow! */ 
